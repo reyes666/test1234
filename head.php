f0ba2c85a9eb980541be722390eb7ea1ec2a74ec
Mathieu <asm@videotron.ca>
2012-07-27 23:53:55 -0400

    Revert "new file"
    This reverts commit acd19361210bb8fb64b7148c2b1423d21a5c4eba.

--- head.php
+++
@@ -1,5 +0,0 @@
-<title><?php echo TXT_TITLE ?></title>
-<meta property="og:title" content="<?php echo TXT_TITLE ?>"/>
-<meta property="og:type" content="article"/>
-<meta property="og:image" content="<?php echo LOGO_URL ?>"/>
-<meta property="og:description" content="<?php echo TXT_DESCRIPTION ?>" />
